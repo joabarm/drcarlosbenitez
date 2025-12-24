8026c1115e9f924a99defcb23ee1c781d2f6fb86
Joaquin <jabarzuam1@gmail.com>
2025-12-24 13:41:56 -0300

    Add pics to showcase work

--- script.js
+++ script.js
@@ -28,11 +28,8 @@ const url_instagram = "https://www.instagram.com/carlosbenitezdentista?igsh=MTY0
 document.querySelectorAll(".ig-link").forEach(el => {el.href = url_instagram});
 
 const igpics = [
-    "https://i.pinimg.com/736x/b0/b4/5f/b0b45f1ba2c7a01d09068f66f9ea998c.jpg",
-    "https://images.cults3d.com/DRAHnPUeQyROSAgepOyZ24pufj8=/516x516/filters:no_upscale():format(webp)/https://fbi.cults3d.com/uploaders/43850820/illustration-file/b62a52f1-d205-456e-b70e-d76334ac786f/pikac.jpeg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvlE3b1px0kqEXmHTPBwHzlI4MDoLlg5NoVw&s"
+    "./assets/igpic1.png",
+    "./assets/igpic2.png",
+    "./assets/igpic3.png",
 ]
 document.querySelectorAll(".igpics").forEach((img, ind) => {img.src = igpics[ind]});
-
-// const url_linkedin = "...";
-// document.getElementById("link-linkedin").href = url_linkedin;
